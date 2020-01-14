a62a432ad210b5d8309d4ba679f9030cc82eb809
Vinícios SIlva <vinicsilva811@gmail.com>
2020-01-14 17:10:38 -0300

    Adicionado suporte ao sensor de distância

--- src/CosmosNV2.h
+++ src/CosmosNV2.h
@@ -23,6 +23,16 @@ Versão: 1.0 Janeiro de 2020
 
 #include <Arduino.h>
 
+// ------------------------------------ CONSTANTES ------------------------------------------ //
+
+#define L1 14
+#define L2 15
+#define L3 16
+#define L4 17
+#define L5 19
+#define L6 24
+#define L7 22
+
 // ------------------------------- Classe SensorDeLinha ------------------------------------- //
 
 class SensorDeLinha
@@ -44,6 +54,20 @@ class SensorDeToque
     boolean lerEstado();
   private:
     uint8_t _porta;
+};
+
+// ----------------------------- Classe SensorDeDistancia ----------------------------------- //
+
+class SensorDeDistancia
+{
+  public:
+    SensorDeDistancia(uint8_t porta);
+    boolean iniciar();
+    int ler();
+  private:
+    uint8_t _porta, shut, endereco;
+    Adafruit_VL53L0X sensor;
+    VL53L0X_RangingMeasurementData_t distancia;
 };
 
 // ------------------------------------------------------------------------------------------ //
